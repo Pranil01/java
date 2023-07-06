9e5aa58cfe8b0049309a767030327caabce8c5ab
Pranil01 <94604003+Pranil01@users.noreply.github.com>
2023-07-06 17:38:59 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,11 @@
+package mypratice;
+
+public class Increment {
+	public static void main(String[]args) {
+		
+		int a = 4;
+		int y = ++a;
+		System.out.println(y);
+	}
+
+}
---
+++ Increment1.java
@@ -0,0 +1,16 @@
+package mypratice;
+
+public class Increment1 {
+
+	public static void main(String[] args) {
+		
+				
+				char a = 'B';
+				char y = ++a;
+				System.out.println(y);
+				int z = ++a;
+				System.out.println(z);
+			}
+
+		}
+	
